8b57afc1a631a927f28b9862d945213435d788e5
SwapnilSankla <swapnil.sankla@gmail.com>
2015-11-01 12:09:38 +0530

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,5 @@
+/**
+ * Created by swapnilsankla on 01/11/15.
+ */
+public class Main {
+}
---
+++ src/test/java/MainTest.java
@@ -0,0 +1,8 @@
+import static org.junit.Assert.*;
+
+/**
+ * Created by swapnilsankla on 01/11/15.
+ */
+public class MainTest {
+
+}
